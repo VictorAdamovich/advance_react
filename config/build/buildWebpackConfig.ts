ed4d37c269f0e5cac8650c6ed824955a4ce77cef
Adamovich <Ndptrv@yandex.by>
2023-08-30 00:24:17 +0300

    feat: decomposition webpack config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,24 @@
+import webpack from 'webpack'
+import path from 'path';
+import { BuildOptions } from './types/config';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildPlugins } from './buildPlugins';
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { mode, paths } = options
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders()
+        },
+        resolve: buildResolvers(),
+    }
+}
