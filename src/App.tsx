5dfaaa4cd010fcdfdf3666887124a0b186c87aaa
Adamovich <Ndptrv@yandex.by>
2023-09-02 00:57:02 +0300

    fear: add rrd and setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import { Suspense } from 'react';
 import { Counter } from "./components/Counter"
+import { Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 
 const App = () => {
   return (
     <div className="app">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        <Counter/>
+      <Link to={'/'}>MainPage</Link>
+      <Link to={'/about'}>AboutPage</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<MainPageAsync />} />
+          <Route path='/about' element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
+      <Counter />
     </div>
   )
 }
