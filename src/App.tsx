ada3f50fe054f1d7e2c6e52aae8355133216ae6a
Adamovich <Ndptrv@yandex.by>
2023-09-06 22:24:53 +0300

    feat: add classNames

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import "./styles/index.scss";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>MainPage</Link>
       <Link to={"/about"}>AboutPage</Link>
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,16 @@
+type Mods = Record<string, boolean | string>
+
+export const classNames = (cls: string, mods: Mods, additional: string[]): string => {
+
+    return [
+        cls,
+        ...additional,
+        ...Object
+            .entries(mods)
+            .filter(([cls, value]) => Boolean(value))
+            .map(([cls]) => cls)
+    ].join(' ');
+
+}
+
+classNames('remove-btn', { hovered: true, selectable: true, red: true }, ['pdg'],)
